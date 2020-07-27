16b651b75c535af67aa97f5c9ba3fe4c1db7ed2b
Nicon <65840719+UKNOWN2@users.noreply.github.com>
2020-07-27 12:18:47 +0530

    Create Metasploit-Install.sh

---
+++ Metasploit-Install.sh
@@ -0,0 +1,31 @@
+clear
+echo " >>>>>>>>>>METASPLOIT INSTALLATION STARTED<<<<<<<<<< "
+echo "To Terminate press ^C"
+sleep 4.0
+termux-setup-storage
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pkg install --uprade pip
+pip install lolcat
+pkg install nano -y
+echo " "
+sleep 2.0
+pkg install wget -y
+pkg install openssh -y
+pkg install ruby -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+sleep 2.0
+cd /data/data/com.termux/files/usr/opt
+ls
+mv metasploit-framework $HOME
+wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt
+bash fix-ruby-bigdecimal.sh.txt
+gem install rubygems-update
+update_rubygems
+gem install -j5
+
+echo " INSTALLATION COMPLETED "
